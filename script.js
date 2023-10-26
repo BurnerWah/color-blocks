92bc358cc74d831eadc47d891c82972311c5d7a5
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-10-26 14:02:26 -0500

    Let blocks be created

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("🟥 🟦 🟩 🟨")
+
+const BLOCKS = document.getElementById("blocks")
+console.log(BLOCKS)
+
+function createRed(e) {
+  const block = createBlock("red")
+  BLOCKS.appendChild(block)
+}
+
+function createBlue(e) {
+  const block = createBlock("blue")
+  BLOCKS.appendChild(block)
+}
+
+function createGreen(e) {
+  const block = createBlock("green")
+  BLOCKS.appendChild(block)
+}
+
+function createYellow(e) {
+  const block = createBlock("yellow")
+  BLOCKS.appendChild(block)
+}
+
+/**
+ * @param {"red"|"blue"|"green"|"yellow"} color
+ * @returns {HTMLDivElement}
+ */
+function createBlock(color) {
+  const block = document.createElement("div")
+  block.classList.add("block", `${color}-fill`)
+  console.log("Created a block:", block)
+  return block
+}
+
+// function appendTo
